d73555f6f425cb03d1ae928253e0cbc6293ad261
Uallesson Nunes Ivo <uallessons@gmail.com>
2021-07-15 10:47:02 -0300

    initial commit

---
+++ src/styles/global-styles.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
