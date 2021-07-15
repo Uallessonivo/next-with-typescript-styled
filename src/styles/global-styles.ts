de01d218797de629dd5a685b6f1e72b46fff3024
Uallesson Nunes Ivo <uallessons@gmail.com>
2021-07-15 11:12:56 -0300

    global styles

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -5,9 +5,26 @@ export const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     padding: 0;
     margin: 0;
+    outline: none;
   }
 
   button {
     cursor: pointer;
+  }
+
+  @media (max-width: 1080px) {
+      html {
+          font-size: 93.75%;
+      }
+  }
+
+  @media (max-width: 720px) {
+      html {
+          font-size: 87.5%;
+      }
+  }
+
+  a {
+      text-decoration: none;
   }
 `
